1f206f752e92b10b7742899c2817d52deef0b365
Peter <designing.developer111@gmail.com>
2024-02-28 00:17:50 +0530

    Commit on machine

---
+++ app/src/main/java/com/devtides/stackoverflow/view/DetailsActivity.kt
@@ -0,0 +1,31 @@
+package com.devtides.stackoverflow.view
+
+import android.content.Context
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.devtides.stackoverflow.R
+import com.devtides.stackoverflow.model.Question
+
+class DetailsActivity : AppCompatActivity() {
+
+    companion object{
+        val PARAM_QUESTION = "params_question"
+
+        fun getIntent(context: Context, question: Question) =
+            Intent(context,DetailsActivity::class.java).putExtra(PARAM_QUESTION,question)
+
+    }
+
+    var question:Question?=null
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_details)
+
+       // question = intent.extras?.getParcelable(PARAM_QUESTION)
+
+        if(question==null){
+            finish()
+        }
+    }
+}
